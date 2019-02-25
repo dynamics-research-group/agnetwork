63243863ffa09d88effad4ef1f371660d18562d1
Julian Gosliga <julian_gosliga@hotmail.com>
2019-02-25 14:48:50 +0000

    Changed element module to include beams and boundary conditions

--- element.py
+++ element.py
@@ -3,9 +3,33 @@ from structure import Structure
 class Element(Structure):
     # Create an instance of elements with user-defined ID and default attributes
     def __init__(self,
-                 elementID,
+                 elementID):
+        self.elementID = elementID
+        Element.elements[elementID] = []
+    
+    def remove(self):
+        try:
+            del Element.elements[self.elementID]
+        except:
+            return None
+
+class Beam(Element):
+    def __init__(self,
+                 beamID,
                  length=1,
                  mass=1):
-        self.elementID = elementID
+        Element.__init__(self, beamID)
         self.length = length
         self.mass = mass
+        Element.elements[beamID] = [mass, length]
+
+class Boundary(Element):
+    # Create an instance of a boundary condition with user-defined ID and default attributes
+    def __init__(self,
+                 boundaryID,
+                 disp=0,
+                 trac=0):
+        Element.__init__(self, boundaryID)
+        self.disp = disp
+        self.trac = trac
+        Element.elements[boundaryID] = [disp, trac]
--- element_test.py
+++ element_test.py
@@ -1,20 +1,62 @@
 import unittest
 from element import Element
+from element import Beam
+from element import Boundary
+from unittest.mock import patch
 
 class TestElementClass(unittest.TestCase):
 
     def setUp(self):
         # Create an instance of the Element class 
-        self.Ea = Element('a')
+        self.earth = Element('A')
 
     def tearDown(self):
-        pass
+        del self.earth
 
     def test_init(self):
         # Check that instance of Element initialised correctly
-        self.assertEqual(self.Ea.elementID, 'a')
-        self.assertEqual(self.Ea.length, 1)
-        self.assertEqual(self.Ea.mass, 1)
+        self.assertEqual(self.earth.elementID, 'A')
+        self.assertEqual(Element.elements['A'], [])
+
+    def test_remove(self):
+        # Test that remove method deletes dictionary entry for element
+        self.earth.remove()
+        with self.assertRaises(KeyError):
+            Element.elements['A']
+        # Check that trying to remove an entry that does not exist returns none
+        self.assertIsNone(self.earth.remove())
+
+class TestBeamClass(unittest.TestCase):
+
+    def setUp(self):
+        # Create an instance of the Element class 
+        self.BeamA = Beam('A')
+
+    def tearDown(self):
+        del self.BeamA
+
+    def test_init(self):
+        # Check that instance of Element initialised correctly
+        self.assertEqual(self.BeamA.elementID, 'A')
+        self.assertEqual(self.BeamA.length, 1)
+        self.assertEqual(self.BeamA.mass, 1)
+        self.assertEqual(Beam.elements['A'], [1, 1])
+
+class TestBoundaryClass(unittest.TestCase):
+
+    def setUp(self):
+        # Create an instance of the Boundary class 
+        self.BC1 = Boundary(1)
+
+    def tearDown(self):
+        del self.BC1
+
+    def test_init(self):
+        # Check that instance of Boundary initialised correctly
+        self.assertEqual(self.BC1.elementID, 1)
+        self.assertEqual(self.BC1.disp, 0)
+        self.assertEqual(self.BC1.trac, 0)
+        self.assertEqual(Boundary.elements[1], [0, 0])
 
 if __name__ == '__main__':
     unittest.main()
