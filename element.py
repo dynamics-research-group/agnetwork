85e3e20197f0cc7709d44c03c56316caa6a0bf9e
Julian Gosliga <julian_gosliga@hotmail.com>
2019-02-25 17:09:49 +0000

    Completed the element unit test and element class

--- element.py
+++ element.py
@@ -1,35 +1,51 @@
 from structure import Structure
 
 class Element(Structure):
-    # Create an instance of elements with user-defined ID and default attributes
+    """Create an instance of elements with user-defined ID"""
     def __init__(self,
                  elementID):
         self.elementID = elementID
         Element.elements[elementID] = []
     
     def remove(self):
+        """Removes the dictionary entry for the elements"""
         try:
             del Element.elements[self.elementID]
         except:
             return None
+    
+    def removeConnections(self):
+        """Removes any joints connected to the element"""
+        for jointID in list(Element.joints.keys()):
+            localJoint = Element.joints[jointID][0]
+            if self.elementID in localJoint:
+                del Element.joints[jointID]
+    
+    def __del__(self):
+        self.removeConnections()
+        self.remove()
 
 class Beam(Element):
+    """Create a beam element with default attributes"""
     def __init__(self,
-                 beamID,
+                 elementID,
                  length=1,
                  mass=1):
-        Element.__init__(self, beamID)
+        Element.__init__(self, elementID)
         self.length = length
         self.mass = mass
-        Element.elements[beamID] = [mass, length]
+        Element.elements[elementID] = [mass, length]
 
 class Boundary(Element):
-    # Create an instance of a boundary condition with user-defined ID and default attributes
+    """Create a boundary condition with default attributes"""
     def __init__(self,
-                 boundaryID,
+                 elementID,
                  disp=0,
                  trac=0):
-        Element.__init__(self, boundaryID)
+        Element.__init__(self, elementID)
         self.disp = disp
         self.trac = trac
-        Element.elements[boundaryID] = [disp, trac]
+        Element.elements[elementID] = [disp, trac]
+
+if __name__ == "__main__":
+    pass
