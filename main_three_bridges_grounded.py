433a1cf423612c7b9f2c7aa7a60d31e853edbe51
Julian Gosliga <julian_gosliga@hotmail.com>
2019-05-14 16:28:58 +0100

    Inserted comments

--- main_three_bridges_grounded.py
+++ main_three_bridges_grounded.py
@@ -8,11 +8,13 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
 
+    # Define the graph for bridge1
     bridge1 = Structure('Bridge1')
     bridge1.graph = {1 : ['A','A','B'],
                     'A': [1, 'B', 1], 
                     'B': ['A', 1]}
 
+    # Define the graph for bridge2
     bridge2 = Structure('Bridge2')
     bridge2.graph = {1: ['A','B','C','D'],
                     'A': [1, 'C', 'B'], 
@@ -20,6 +22,7 @@ if __name__ == '__main__':
                     'B': ['A', 1], 
                     'D': ['C', 1]}
 
+    # Define the graph for bridge3
     bridge3 = Structure('Bridge3')
     bridge3.graph = {1: ['A'], 
                      2: ['B'], 
@@ -33,18 +36,25 @@ if __name__ == '__main__':
                     'E': ['C','F', 5],
                     'F': ['E', 4]}
     
+    # Initiliase nx.Graph object
     bridge1Graph = nx.Graph()
+    # Add nodes and edges from bridge1
     bridge1Graph.add_nodes_from(bridge1.nodeList())
     bridge1Graph.add_edges_from(bridge1.edgeList())
 
+    # Initiliase nx.Graph object
     bridge2Graph = nx.Graph()
+    # Add nodes and edges from bridge2    
     bridge2Graph.add_nodes_from(bridge2.nodeList())
     bridge2Graph.add_edges_from(bridge2.edgeList())
     
+    # Initiliase nx.Graph object
     bridge3Graph = nx.Graph()
+    # Add nodes and edges from bridge3
     bridge3Graph.add_nodes_from(bridge3.nodeList())
     bridge3Graph.add_edges_from(bridge3.edgeList())
 
+    # Create subplots with bridge graphs
     plt.subplot(121)
     nx.draw(bridge1Graph, with_labels=True)
     plt.subplot(122)
