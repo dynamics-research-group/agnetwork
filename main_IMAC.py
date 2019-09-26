54fe6864fb14856f8509e0323804ba4e01be268e
Julian Gosliga <julian_gosliga@hotmail.com>
2019-09-26 16:12:21 +0100

    Added bridges to IMAC code

--- main_IMAC.py
+++ main_IMAC.py
@@ -45,7 +45,7 @@ if __name__ == '__main__':
     turbine1.edgeList()
 
     # Define the graph for an 747
-    aeroplane1 = Structure('Aeroplane1')
+    aeroplane1 = Structure('Boeing-747')
     aeroplane1.elements = {'A1': ['FRP',      'Truncated cone'],
                            'A2': ['FRP',      'Cylindrical beam'],
                            'A3': ['FRP',      'Cone'],
@@ -90,51 +90,82 @@ if __name__ == '__main__':
     aeroplane1.edgeList()
 
     # Define the graph for a Cessna 172
-    aeroplane2 = Structure('Aeroplane1')
-    aeroplane2.elements = {'A1': ['FRP',      'Truncated cone'],
-                           'A2': ['FRP',      'Cylindrical beam'],
-                           'A3': ['FRP',      'Cone'],
-                           'B' : ['FRP',      'Aerofoil'], 
-                           'C' : ['FRP',      'Complex'],
-                           'D' : ['Assembly', 'Cylinder'],
-                           'E' : ['FRP',      'Complex'],
-                           'F' : ['Assembly', 'Cylinder'],
-                           'G' : ['FRP',      'Aerofoil'],
-                           'H' : ['FRP',      'Complex'],
-                           'I' : ['Assembly', 'Cylinder'],
-                           'J' : ['FRP',      'Complex'],
-                           'K' : ['Assembly', 'Cylinder'],
-                           'L' : ['FRP',      'Aerofoil'], 
-                           'M' : ['FRP',      'Aerofoil'],
-                           'N' : ['FRP',      'Aerofoil'],
-                           'O' : ['Assembly', 'Assembly'],
-                           'P' : ['Assembly', 'Assembly'],
+    aeroplane2 = Structure('Cessna-172')
+    aeroplane2.elements = {'A' : ['Aluminium',  'Aerofoil'],
+                           'B' : ['Aluminium',  'Aerofoil'],
+                           'C' : ['Aluminium',  'Complex'],
+                           'D1': ['FRP',        'Rectangular shell'], 
+                           'D2': ['Mixed',      'Trapezoid'],
+                           'D3': ['FRP',        'Trapezoid'],
+                           'E' : ['FRP',        'Aerofoil'],
+                           'F' : ['FRP',        'Aerofoil'],
+                           'G' : ['Aluminium',  'Cylindrical beam'],
+                           'H' : ['Aluminium',  'Cylindrical beam'],
+                           'I' : ['FRP',        'Aerofoil'],
+                           'J' : ['FRP',        'Aerofoil'],
+                           'K' : ['FRP',        'Aerofoil'],
+                           'L' : ['Assembly',   'Assembly'], 
+                           'M' : ['Assembly',   'Assembly'],
                            '1' : ['Ground']}
 
-    aeroplane2.joints = {'1':  [['A1','A2'], [34.2, 14.68, 5.165], 'Perfect'],
-                         '2':  [['A2','A3'], [34.2, 60.96, 5.165], 'Perfect'],
-                         '3':  [['A2','B'],  [32.2, 29.79, 2.89],  'Lug'],
-                         '4':  [['B', 'C'],  [13.2, 42.67, 4.74],  'Complex'],
-                         '5':  [['C', 'D'],  [13.2, 40.17, 4.74],  'Complex'],
-                         '6':  [['B', 'E'],  [23.2, 30.79, 3.57],  'Complex'],
-                         '7':  [['E', 'F'],  [23.2, 28.29, 3.57],  'Complex'],
-                         '8':  [['A2','G'],  [36.2, 29.79, 2.89],  'Lug'],
-                         '9':  [['G', 'H'],  [45.2, 30.79, 3.57],  'Complex'],
-                         '10': [['H', 'I'],  [45.2, 28.29, 3.57],  'Complex'],
-                         '11': [['G', 'J'],  [55.2, 42.67, 4.74],  'Complex'],
-                         '12': [['J', 'K'],  [55.2, 40.17, 4.74],  'Complex'],
-                         '13': [['A3','L'],  [33.2, 68.58, 7.55],  'Lug'],
-                         '14': [['A3','M'],  [35.2, 68.58, 7.55],  'Lug'],
-                         '15': [['A3','N'],  [34.2, 64.58, 9.16],  'Lug'],
-                         '16': [['A1','O'],  [34.2, 7.75,  1.75],  'Complex'],
-                         '17': [['A2','P'],  [34.2, 29.67, 1.75],  'Complex'],
-                         '18': [['O', '1'],  [34.2, 7.75,  0],     'Friction'],
-                         '19': [['P', '1'],  [34.2, 29.67, 0],     'Friction']}
+    aeroplane2.joints = {'1':  [['A', 'C'],   [0, 0, 0], 'Bolted'],
+                         '2':  [['B', 'C'],   [0, 0, 0], 'Bolted'],
+                         '3':  [['C', 'D1'],  [0, 0, 0], 'Lug'],
+                         '4':  [['D1', 'D2'], [0, 0, 0], 'Perfect'],
+                         '5':  [['D1', 'D3'], [0, 0, 0], 'Perfect'],
+                         '6':  [['E', 'D2'],  [0, 0, 0], 'Complex'],
+                         '7':  [['F', 'D2'],  [0, 0, 0], 'Complex'],
+                         '8':  [['D3', 'I'],  [0, 0, 0], 'Lug'],
+                         '9':  [['D3', 'J'],  [0, 0, 0], 'Complex'],
+                         '10': [['D3', 'K'],  [0, 0, 0], 'Complex'],
+                         '11': [['D1', 'L'],  [0, 0, 0], 'Complex'],
+                         '12': [['D1', 'M'],  [0, 0, 0], 'Complex'],
+                         '13': [['D1', 'G'],  [0, 0, 0], 'Pinned'],
+                         '14': [['D1', 'H'],  [0, 0, 0], 'Pinned'],
+                         '15': [['E', 'G'],   [0, 0, 0], 'Pinned'],
+                         '16': [['F', 'H'],   [0, 0, 0], 'Pinned'],
+                         '17': [['L', '1'],   [0, 0, 0], 'Friction'],
+                         '18': [['M', '1'],   [0, 0, 0], 'Friction']}
+    
     aeroplane2.addElements()
     aeroplane2.addJoints()
     aeroplane2.edgeList()
 
-    graph1 = aeroplane1
+    bridge1 = Structure('Bridge1')
+    bridge1.graph = {'1' : ['A'],
+                     '2' : ['B'],
+                     '3' : ['A'], 
+                     'A': ['1', 'B', '3'], 
+                     'B': ['A', '2']}
+
+    bridge2 = Structure('Bridge2')
+    bridge2.graph = {'1': ['A'], 
+                     '2': ['B'], 
+                     '3': ['D'],
+                     '4': ['C'], 
+                     'A': ['1', 'C', 'B'], 
+                     'C': ['A', 'D', '4'],
+                     'B': ['A', '2'], 
+                     'D': ['C', '3']}
+
+    bridge3 = Structure('Bridge3')
+    bridge3.graph = {'1': ['A'], 
+                     '2': ['B'], 
+                     '3': ['D'],
+                     '4': ['F'], 
+                     '5': ['E'], 
+                     'A': ['1', 'C', 'B'], 
+                     'C': ['A', 'D', 'E'],
+                     'B': ['A', '2'], 
+                     'D': ['C', '3'],
+                     'E': ['C','F', '5'],
+                     'F': ['E', '4']}
+    
+    bridge1.addToNetwork()
+    bridge2.addToNetwork()
+    bridge3.addToNetwork()
+
+    graph1 = bridge3
     graph2 = aeroplane2
     
     print("Turbine nodes:", graph1.nodes)
@@ -254,7 +285,6 @@ if __name__ == '__main__':
     nx.draw(graph2nx, with_labels=True, pos=nx.spring_layout(graph2nx))
     plt.show()
 
-    """
     mcs = max_cliques[0]
     subgraph_edges = []
     for v1 in mcs:
@@ -268,5 +298,3 @@ if __name__ == '__main__':
 
     nx.draw(subgraph, with_labels=True)
     plt.show()
-
-    """
