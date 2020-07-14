d2751b25517ed2577c28507c54f3ef965e819d41
Julian Gosliga <julian_gosliga@hotmail.com>
2020-07-14 13:04:28 +0100

    import elements correctly

--- main/IEimport/IEimport.py
+++ main/IEimport/IEimport.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import json
 import time
+import re
 
 timestamp_conversion_factor = 10**9
 json_export_directory = "/Users/Julian/Documents/WorkDocuments/Irreducible Element/IE models/json"
@@ -9,11 +10,8 @@ def import_IE_from_excel(structure, file_path, population=None):
 	if structure == None or file_path == None: return 
 
 	name = structure
-
 	timestamp = time.time() * timestamp_conversion_factor
-
 	structure_dict = {"name": name,
-					  "population": population,
 					  "timestamp": int(timestamp),
 					  "irreducible_element_model": {
 						  "number_of_elements": 0,
@@ -23,21 +21,46 @@ def import_IE_from_excel(structure, file_path, population=None):
 						  "joints": []
 					  	}
 					 }
+	if population != None:
+		structure_dict["population"] = population
 
-	elements = pd.read_excel (file_path, sheet_name='Elements')
-	elements_json_str = elements.to_json(indent=2).lower()
-	elements_json_dict = json.loads(elements_json_str)
-	
-	# print('Excel Sheet to JSON:\n', elements_json_str)
+	elements_raw = pd.read_excel(file_path, sheet_name='Elements')
+	elements = elements_raw.loc[:, ~elements_raw.columns.str.contains('^Unnamed')].dropna()
+	elements_json_str = elements.to_json(indent=2, orient="records").lower()
+	elements_dict = json.loads(elements_json_str)
 
-	if "name" in elements_json_dict:
-		for i, element in enumerate(elements_json_dict["name"].values()):
-			element_dict = {"name": f"element-{i}",
-							"description": element,
-							"type": "regular",
-							"metadata": {}}
-			structure_dict["irreducible_element_model"]["elements"].append(element_dict)
-			print('Excel Sheet to JSON:\n', element)
+	for element in elements_dict:
+		element_dict = {}
+		material_dict = {}
+		shape_dict = {}
+		# element_properties = ["description", 
+		# 					  "name", 
+		# 					  "material", 
+		# 					  "material class",
+		# 					  "material properties",
+		# 					  "geometry class", 
+		# 					  "shape",
+		# 					  "dimensions"]
+		for key, value in element.items():
+			print(f"Key:{key}, value: {value}")
+			if "description" in key: element_dict["description"] = value
+			if "name" in key: element_dict["name"] = value
+			if "material" in key:
+				if "class" not in key and "properties" not in key: 
+					material_dict["name"] = value
+			if "material class" in key: material_dict["class"] = value
+			if "material properties" in key: material_dict["properties"] = return_properties_object(value)
+			if "shape" in key: shape_dict["name"] = value
+			if "geometry class" in key: shape_dict["class"] = value
+			if "dimensions" in key: shape_dict["dimensions"] = return_properties_object(value)
+		
+		element_dict["material"] = material_dict
+		element_dict["shape"] = shape_dict
+					
+		element_dict["metadata"] = {}
+		element_dict["type"] = "regular"
+
+		structure_dict["irreducible_element_model"]["elements"].append(element_dict)
 
 	# joints = pd.read_excel (file_path, sheet_name='Joints')
 	# boundary_conditions = pd.read_excel (file_path, sheet_name='Boundary conditions')
@@ -45,8 +68,33 @@ def import_IE_from_excel(structure, file_path, population=None):
 	with open (f"{json_export_directory}/{structure}.json", "w") as outfile:
 		json.dump(structure_dict, outfile, indent=4)
 
+def return_properties_object(properties):
+	properties_object = []
+	split_properties = re.split(",", properties)
+	for p in split_properties:
+		split = re.split(":", p.strip())
+		name = split[0]
+		to_split = split[1].strip()
+		for i, c in reversed(list(enumerate(to_split))):
+			if c.isdigit():
+				break	
+		value=float(to_split[:i+1])
+		units=to_split[i+1:]
+		# print(f"Name {name}, value: {value}, units:{units}")
+		if units == '':
+			properties_object.append({"name": name,
+								      "value": value})
+		else: 
+			properties_object.append({"name": name,
+								  	  "value": value,
+								      "units": units})
+	return properties_object
+
+
 if __name__ == "__main__":
-	import_IE_from_excel('Aeroplane 1', 
-						 "/Users/Julian/Documents/WorkDocuments/Irreducible Element/IE models/Excel/Aeroplane 1.xlsx")
+	import_IE_from_excel('IE example', 
+						 "/Users/Julian/Documents/WorkDocuments/Irreducible Element/IE models/Excel/IE example.xlsx")
+	# import_IE_from_excel('Aeroplane 1', 
+	# 					 "/Users/Julian/Documents/WorkDocuments/Irreducible Element/IE models/Excel/Aeroplane 1.xlsx")
 	# import_IE_from_excel('Bridge 1', 
 	# 					 "/Users/Julian/Documents/WorkDocuments/Irreducible Element/IE models/Excel/Bridge 1.xlsx")
